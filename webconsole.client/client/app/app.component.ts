8e9003215ba52e5459920859e83fb7aa01776463
Carsten Gräf <evandor@gmail.com>
2016-06-19 08:50:37 +0200

    wip

--- webconsole.client/client/app/app.component.ts
+++ webconsole.client/client/app/app.component.ts
@@ -22,32 +22,32 @@ import {D3Directive} from './directives/d3.directive'
 import {PercentBarDirective} from './directives/percentBar.d3.directive'
 
 @RouteConfig([
-    { path: '/framework',    as: 'Framework', component: FrameworkComponent },
-    { path: '/bundles',      as: 'Bundles',   component: BundlesComponent, useAsDefault: true },
-    { path: '/bundles/:id',  as: 'Bundle',    component: BundleComponent },
-    { path: '/services',     as: 'Services',  component: ServicesComponent },
-    { path: '/services/:id', as: 'Service',   component: ServiceComponent },
-    { path: '/packages',     as: 'Packages',  component: PackagesComponent },
-    { path: '/logs',         as: 'Logs',      component: LogsComponent },
-   // { path: '/graph',        as: 'Graph',     component: GraphComponent },
-    { path: '/snapshots',    as: 'Snapshots', component: SnapshotsComponent },
-    { path: '/help',         as: 'Help',      component: HelpComponent }
+    { path: '/framework', as: 'Framework', component: FrameworkComponent },
+    { path: '/bundles', as: 'Bundles', component: BundlesComponent, useAsDefault: true },
+    { path: '/bundles/:id', as: 'Bundle', component: BundleComponent },
+    { path: '/services', as: 'Services', component: ServicesComponent },
+    { path: '/services/:id', as: 'Service', component: ServiceComponent },
+    { path: '/packages', as: 'Packages', component: PackagesComponent },
+    { path: '/logs', as: 'Logs', component: LogsComponent },
+    // { path: '/graph',        as: 'Graph',     component: GraphComponent },
+    { path: '/snapshots', as: 'Snapshots', component: SnapshotsComponent },
+    { path: '/help', as: 'Help', component: HelpComponent }
 ])
 @Component({
     selector: 'my-app',
     templateUrl: 'app/html/app.template.html',
-    directives: [RouterOutlet,RouterLink, FooterComponent,Navbar,Breadcrumbs, BarGraph,TimerWebsocketComponent, SMDropdown,D3Directive,PercentBarDirective]
+    directives: [RouterOutlet, RouterLink, FooterComponent, Navbar, Breadcrumbs, BarGraph, TimerWebsocketComponent, SMDropdown, D3Directive, PercentBarDirective]
 })
 export class AppComponent {
-    
+
     name: string = "Ringo";
-  names: string[] = ["John", "Paul", "George", "Ringo"];
-    
+    names: string[] = ["John", "Paul", "George", "Ringo"];
+
     graphData: Array<number>;
     data: Array<number>;
-    
+
     constructor() {
-        this.graphData = [10,20,30,40,60];
+        this.graphData = [10, 20, 30, 40, 60];
         this.data = [4, 8, 15, 16, 23, 42];
     }
- }
+}
--- webconsole/test/io/skysail/webconsole/server/handler/test/ServiceHandlerTest.java
+++ webconsole/test/io/skysail/webconsole/server/handler/test/ServiceHandlerTest.java
@@ -23,56 +23,56 @@ import io.skysail.webconsole.test.TestUtils;
 
 public class ServiceHandlerTest {
 
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
-	private ServiceHandler serviceHandler;
-	private IHTTPSession session;
-	private BundleContext bundleContext;
+    private ServiceHandler serviceHandler;
+    private IHTTPSession session;
+    private BundleContext bundleContext;
 
+    @Before
+    public void setup() {
+        bundleContext = Mockito.mock(BundleContext.class);
+        serviceHandler = new ServiceHandler(bundleContext);
+        session = Mockito.mock(IHTTPSession.class);
+        Mockito.when(session.getMethod()).thenReturn(Method.GET);
+    }
 
-	@Before
-	public void setup() {
-		bundleContext = Mockito.mock(BundleContext.class);
-		serviceHandler = new ServiceHandler(bundleContext);
-		session = Mockito.mock(IHTTPSession.class);
-		Mockito.when(session.getMethod()).thenReturn(Method.GET);
-	}
+    @Test
+    public void throws_NullPointerException_for_unknown_serviceId() {
+        thrown.expect(NullPointerException.class);
+        Mockito.when(bundleContext.getBundles()).thenReturn(new Bundle[0]);
+        Mockito.when(session.getUri()).thenReturn("/services/6");
 
-	@Test
-	public void throws_NullPointerException_for_unknown_serviceId() {
-		thrown.expect(NullPointerException.class);
-		Mockito.when(bundleContext.getBundles()).thenReturn(new Bundle[0]);
-		Mockito.when(session.getUri()).thenReturn("/services/6");
+        serviceHandler.handle(session);
+    }
 
-		serviceHandler.handle(session);
-	}
+    @Test
+    public void test() throws InvalidSyntaxException {
 
-	@Test
-	public void test() throws InvalidSyntaxException {
+        Bundle theBundle = TestUtils.mockBundle(0L, "symbolicName", "0.1.0");
 
-		Bundle theBundle = TestUtils.mockBundle(0L, "symbolicName", "0.1.0");
+        ServiceReference<?>[] serviceReferences = new ServiceReference[1];
+        ServiceReference<?> serviceRef = Mockito.mock(ServiceReference.class);
+        serviceReferences[0] = serviceRef;
 
-		ServiceReference<?>[] serviceReferences = new ServiceReference[1];
-		ServiceReference<?> serviceRef = Mockito.mock(ServiceReference.class);
-		serviceReferences[0] = serviceRef;
+        Mockito.when(bundleContext.getAllServiceReferences(null, "(service.id=1)")).thenReturn(serviceReferences);
+        Mockito.when(session.getUri()).thenReturn("/services/1");
 
-		Mockito.when(bundleContext.getAllServiceReferences(null, "(service.id=1)")).thenReturn(serviceReferences);
-		Mockito.when(session.getUri()).thenReturn("/services/1");
+        Mockito.when(serviceRef.getProperty(Constants.SERVICE_ID)).thenReturn(1L);
+        Mockito.when(serviceRef.getProperty(Constants.OBJECTCLASS))
+                .thenReturn(new String[] { "org.osgi.service.resolver.Resolver" });
+        Mockito.when(serviceRef.getBundle()).thenReturn(theBundle);
+        Mockito.when(serviceRef.getPropertyKeys()).thenReturn(new String[0]);
 
-		Mockito.when(serviceRef.getProperty(Constants.SERVICE_ID)).thenReturn(1L);
-		Mockito.when(serviceRef.getProperty(Constants.OBJECTCLASS)).thenReturn(new String[] {"org.osgi.service.resolver.Resolver"});
-		Mockito.when(serviceRef.getBundle()).thenReturn(theBundle);
-		Mockito.when(serviceRef.getPropertyKeys()).thenReturn(new String[0]);
+        Response response = serviceHandler.handle(session);
 
-		Response response = serviceHandler.handle(session);
-
-		assertThat(response.getStatus().getDescription(), is("200 OK"));
-		assertThat(response.getMimeType().toString(), is("application/json"));
-		String responseAsText = TestUtils.convertStreamToString(response.getData());
-		assertThat(responseAsText, containsString("\"version\":\"0.1.0\""));
-		assertThat(responseAsText, containsString("\"id\":\"1\""));
-		assertThat(responseAsText, containsString("\"objectClass\":\"org.osgi.service.resolver.Resolver\""));
-	}
+        assertThat(response.getStatus().getDescription(), is("200 OK"));
+        assertThat(response.getMimeType().toString(), is("application/json"));
+        String responseAsText = TestUtils.convertStreamToString(response.getData());
+        assertThat(responseAsText, containsString("\"version\":\"0.1.0\""));
+        assertThat(responseAsText, containsString("\"id\":\"1\""));
+        assertThat(responseAsText, containsString("\"objectClass\":\"org.osgi.service.resolver.Resolver\""));
+    }
 
 }
