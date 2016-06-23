634d1c751a3c6a88f0dd592e17fe9f163f68e7e6
Carsten Gräf <evandor@gmail.com>
2016-06-23 16:04:14 +0200

    sed escapes

--- release.sh
+++ release.sh
@@ -1,8 +1,9 @@
 echo "Bundle: $1"
-VERSION="grep Bundle-Version: $1/bnd.bnd | sed 's/Bundle-Version://' | sed 's/.${tstamp}//' | sed -e 's/^[[:space:]]*//'"
+VERSION="grep Bundle-Version: $1/bnd.bnd | sed 's/Bundle-Version://' | sed 's/\.\${tstamp}//' | sed -e 's/^[[:space:]]*//'"
 echo "Version: $VERSION"
 
 git add -A
 git commit -m "new Release ${BUNDLE}-$VERSION"
 
+echo "cp cnf/release/$1/$1-$VERSION.jar /home/carsten/tmp/bundles4release"
 cp cnf/release/$1/$1-$VERSION.jar /home/carsten/tmp/bundles4release
