23005be5f9bb4085c1c82275eb79e9fc0b6a87ba
Carsten Gräf <evandor@gmail.com>
2016-06-23 15:57:16 +0200

    wip

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
-echo "Bundle: $BUNDLE ${BUNDLE}"
-VERSION="grep Bundle-Version: ${BUNDLE}/bnd.bnd | sed 's/Bundle-Version://' | sed 's/.${tstamp}//' | sed -e 's/^[[:space:]]*//'"
+echo "Bundle: $1"
+VERSION="grep Bundle-Version: $1/bnd.bnd | sed 's/Bundle-Version://' | sed 's/.${tstamp}//' | sed -e 's/^[[:space:]]*//'"
 echo "Version: $VERSION"
 
 git add -A
 git commit -m "new Release ${BUNDLE}-$VERSION"
 
-cp cnf/release/${BUNDLE}/${BUNDLE}-$VERSION.jar /home/carsten/tmp/bundles4release
+cp cnf/release/$1/$1-$VERSION.jar /home/carsten/tmp/bundles4release
