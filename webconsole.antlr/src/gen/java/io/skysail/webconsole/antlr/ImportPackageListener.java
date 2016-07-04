04a6fbe2915fc8268a5cac73e5731a406aa3d121
Carsten Gräf <evandor@gmail.com>
2016-07-04 11:03:18 +0200

    new version

--- webconsole.antlr/src/gen/java/io/skysail/webconsole/antlr/ImportPackageListener.java
+++ webconsole.antlr/src/gen/java/io/skysail/webconsole/antlr/ImportPackageListener.java
@@ -1,4 +1,4 @@
-// Generated from /Users/carsten/git/skysail-webconsole/webconsole.antlr/src/main/antlr4/io/skysail/webconsole/antlr/ImportPackage.g4 by ANTLR 4.5.3
+// Generated from C:\git\skysail-webconsole\webconsole.antlr\src\main\antlr4\io\skysail\webconsole\antlr\ImportPackage.g4 by ANTLR 4.5.3
 package io.skysail.webconsole.antlr;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
