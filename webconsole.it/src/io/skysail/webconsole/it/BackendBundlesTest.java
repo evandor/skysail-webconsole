1e5e47dc97c5339c8dffcdc6684be51b6909dabe
Carsten Gräf <evandor@gmail.com>
2016-07-26 09:55:02 +0300

    wip

--- webconsole.it/src/io/skysail/webconsole/it/BackendBundlesTest.java
+++ webconsole.it/src/io/skysail/webconsole/it/BackendBundlesTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
@@ -27,7 +26,6 @@ public class BackendBundlesTest {
     }
 
     @Test
-    @Ignore
     public void testName() throws Exception {
         String bundlespage = browser.getBundles();
         System.out.println(bundlespage);
--- webconsole.it/src/io/skysail/webconsole/it/Browser.java
+++ webconsole.it/src/io/skysail/webconsole/it/Browser.java
@@ -1,6 +1,8 @@
 package io.skysail.webconsole.it;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -12,6 +14,20 @@ public class Browser {
     public Browser() {
         baseUrl = "http://localhost:2002/";
         webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        webClient.setCssErrorHandler(new SilentCssErrorHandler());
+
+        webClient.getOptions().setCssEnabled(true);
+        webClient.getOptions().setRedirectEnabled(false);
+        webClient.getOptions().setAppletEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.getOptions().setPopupBlockerEnabled(true);
+        webClient.getOptions().setTimeout(10000);
+
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(true);
+        webClient.getOptions().setThrowExceptionOnScriptError(true);
+        webClient.getOptions().setPrintContentOnFailingStatusCode(true);
+
     }
 
     public String getBundles() throws Exception {
